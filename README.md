d6a5f72825314c3c7411a77ba07f38b570540f34
thirumal2901 <57386957+thirumal2901@users.noreply.github.com>
2023-02-21 15:25:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-API-chemistry-json
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-yfb5kx)
---
+++ index.js
@@ -0,0 +1,47 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+// run `node index.js` in the terminal
+
+var fs = require('fs');
+var data = fs.readFileSync('chemistry.json');
+var elements = JSON.parse(data);
+const express = require('express');
+const app = express();
+//const cors = require('cors');
+
+app.listen(process.env.PORT, () => console.log('Server Start at 5000 Port'));
+
+app.use(express.static('public'));
+//app.use(cors());
+app.get('/elements', alldata);
+function alldata(request, response) {
+  response.send(elements);
+}
+app.get('/elements/:element/', searchElement);
+function searchElement(request, response) {
+  var word = request.params.element;
+  word = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  console.log(word);
+  //console.log(elements[word]);
+  if (elements[word]) {
+    var reply = elements[word];
+  } else {
+    var reply = {
+      status: 'Not Found',
+    };
+  }
+  console.log(reply.boil);
+  response.send(reply);
+}
+
+console.log(`Hello Node.js v${process.versions.node}!`);
